48207f0ce553a0daef0378750d9ba0d5f1f25225
Quin Cogdell <quincogdell@gmail.com>
2013-12-06 14:56:51 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HashLoveApp::Application.config.secret_token = 'a1f9d3aa00580a21dc7e1714076ead6b928527b2fc30add98af8e0a61bb180cb511b06a0840a9947cf276b0395f7d74cc1fcfead3dd2a7b29a4f70e96545390b'
